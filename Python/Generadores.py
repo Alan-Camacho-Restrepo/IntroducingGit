267f1dce8fb5f046ab5815ad92ca0ad3239a970c
Alan <alanstiv2000@gmail.com>
2021-07-31 11:44:44 -0500

    V1 python

---
+++ Python/Generadores.py
@@ -0,0 +1,24 @@
+def generaPares(n):
+
+	num = 0
+	L = []
+	while num<n:
+		L.append(num*2)
+		num+=1
+
+	return L
+
+print(generaPares(10))
+
+
+def Gene(l):
+	i=1
+	while i<l:
+		yield i*2
+		i+=1
+
+Obj = Gene(10)
+
+print(next(Obj))
+print(next(Obj))
+print(next(Obj))
